08e03919f2ba93c42a6dd9a8bd49f34d0d0bff9a
Àlex Serra <bounteous17@protonmail.com>
2023-11-21 03:20:35 +0100

    undo build output

--- src/mongodb-exercises-resources/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = void 0;
-exports.environment = {
-    mongodb: {
-        uri: 'mongodb://root:toor@localhost:27017/dimatica?authSource=admin',
-    },
-};
